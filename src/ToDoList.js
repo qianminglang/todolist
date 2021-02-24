12776d5001549d67248d4ac4a21ecff17af1dfd3
qianminglang <569040804@qq.com>
2021-02-24 21:42:19 +0800

    todolist功能实现

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,9 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 class ToDoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
   render () {
     return (
-      <div><input></input><button>提交</button></div>
+      <Fragment>
+        <div>
+          {/* 下面是input框 */}
+          <input
+            value={this.state.inputValue}
+            onChange={this.handlerInputChange.bind(this)}
+          />
+          <button onClick={this.buttonClick.bind(this)}>提交</button></div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={index} onClick={this.itemRemove.bind(this, index)}>{item}</li>
+            })
+          }
+        </ul>
+      </Fragment>
     )
   }
+
+  handlerInputChange (e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  buttonClick (e) {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  itemRemove (index) {
+    const list = [...this.state.list]
+    list.splice(index, 1);
+    this.setState({
+      list: list
+    })
+  }
+
 }
 export default ToDoList;
